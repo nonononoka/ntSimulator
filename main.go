466c39cde4a0af6d0e7283f2f92ac00008429c72
nonononoka <onon.kaori@gmail.com>
2026-05-02 13:49:14 +0900

    演習2

--- main.go
+++ main.go
@@ -18,18 +18,39 @@ func main() {
 		fmt.Println(err2)
 		return
 	}
-	s1 := network.NewSwitch(nes, 3)
+	n3, err2 := network.NewNode(3, "00:1A:2B:3C:4D:5C", nes)
+	if err2 != nil {
+		fmt.Println(err2)
+		return
+	}
+	n4, err2 := network.NewNode(4, "00:1A:2B:3C:4D:5D", nes)
+	if err2 != nil {
+		fmt.Println(err2)
+		return
+	}
+	s1 := network.NewSwitch(nes, 5)
+	s2 := network.NewSwitch(nes, 6)
 	l1 := network.NewLink(s1, n1, 100000, 0.001, 0.0, nes)
 	l2 := network.NewLink(s1, n2, 100000, 0.001, 0.0, nes)
+	l3 := network.NewLink(s2, n3, 100000, 0.001, 0.0, nes)
+	l4 := network.NewLink(s2, n4, 100000, 0.001, 0.0, nes)
+	// l5 := network.NewLink(s1, s2, 100000, 0.001, 0.0, nes)
 
 	s1.UpdateForwardingTable(n1.Address(), l1) // n1にいくならl1のリンク
 	s1.UpdateForwardingTable(n2.Address(), l2) // n2にいくならl2のリンク
+	// s1.UpdateForwardingTable(s2.Address(), l5) // n2にいくならl2のリンク
+	s2.UpdateForwardingTable(n3.Address(), l3) // n3にいくならl3のリンク
+	s2.UpdateForwardingTable(n4.Address(), l4) // n3にいくならl3のリンク
 	n1.PrintNode()
 	n2.PrintNode()
+	n3.PrintNode()
+	n4.PrintNode()
 	l1.PrintLink()
 	l2.PrintLink()
+	l3.PrintLink()
+	l4.PrintLink()
 
-	n1.SetTraffic("00:1A:2B:3C:4D:5F", 8000, 1.0, 10.0, 40.0, 85.0, 1.0)
+	n1.SetTraffic("00:1A:2B:3C:4D:5G", 8000, 1.0, 10.0, 40.0, 85.0, 1.0)
 	nes.Visualize()
 	nes.Run()
 	nes.PrintPacketLogs()
