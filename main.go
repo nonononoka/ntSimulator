28dc735ead598eb2a0b060f4e8b9ed05dd9a1864
nonononoka <onon.kaori@gmail.com>
2026-05-02 14:30:50 +0900

    スイッチからスイッチにもいける

--- main.go
+++ main.go
@@ -30,13 +30,13 @@ func main() {
 	}
 	s1 := network.NewSwitch(nes, 5)
 	s1.PrintForwadingTable()
-	// s2 := network.NewSwitch(nes, 6)
+	s2 := network.NewSwitch(nes, 6)
 	// s1を介して，n1~n4が繋がっている
 	l1 := network.NewLink(s1, n1, 100000, 0.001, 0.0, nes)
 	l2 := network.NewLink(s1, n2, 100000, 0.001, 0.0, nes)
-	l3 := network.NewLink(s1, n3, 100000, 0.001, 0.0, nes)
-	l4 := network.NewLink(s1, n4, 100000, 0.001, 0.0, nes)
-	// l5 := network.NewLink(s1, s2, 100000, 0.001, 0.0, nes)
+	l3 := network.NewLink(s2, n3, 100000, 0.001, 0.0, nes)
+	l4 := network.NewLink(s2, n4, 100000, 0.001, 0.0, nes)
+	network.NewLink(s1, s2, 100000, 0.001, 0.0, nes)
 
 	// s1.UpdateForwardingTable(n1.Address(), l1) // n1にいくならl1のリンク
 	// s1.UpdateForwardingTable(n2.Address(), l2) // n2にいくならl2のリンク
@@ -53,10 +53,11 @@ func main() {
 	l4.PrintLink()
 
 	n1.SetTraffic("00:1A:2B:3C:4D:5D", 8000, 1.0, 10.0, 40.0, 85.0, 1.0)
-	n2.SetTraffic("00:1A:2B:3C:4D:5E", 8000, 40.0, 10.0, 40.0, 85.0, 1.0)
+	// n2.SetTraffic("00:1A:2B:3C:4D:5E", 8000, 40.0, 10.0, 40.0, 85.0, 1.0)
 	nes.Visualize()
 	nes.Run()
 	nes.PrintPacketLogs()
 	nes.GenerateSummary()
 	s1.PrintForwadingTable()
+	s2.PrintForwadingTable()
 }
